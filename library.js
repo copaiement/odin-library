c71794fcd7e29963abfc270123858bef43ccf04e
copaiement <cole.paiement@gmail.com>
2023-03-09 12:30:09 -0800

    Multiple updates to html structuring and CSS. Start JS.

--- library.js
+++ library.js
@@ -1,18 +1,54 @@
 // Library JS code
 
+// event listener for add book
+const addBookBtn = document.querySelector('.add-book');
+addBookBtn.addEventListener('click', openModal());
+
 let myLibrary = [];
 
 // Constructor for making book objects
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
   this.title = title,
   this.author = author,
-  this.pages = pages,
-  this.read = read,
+  this.pages = pages
+}
+
+function openModal() {
+  const addButton = document.querySelector('.add-book');
+  const modal = document.querySelector('.modal');
+  
+  modal.classList.remove('hidden');
+  addButton.classList.add('hidden')
 }
 
 function addBookToLibrary() {
+  console.log('test');
+  const title = document.querySelector('#title-input').value;
+  const author = document.querySelector('#author-input').value;
+  const pages = document.querySelector('#pages-input').value;
 
+  const newBook = new Book(title, author, pages);
+  myLibrary[myLibrary.length - 1] = newBook;
+}
+
+function setStatus() {
+  const status = document.querySelector('#status-btn');
+  if (status.getAttribute('class') === 'read') {
+    status.classList.remove('read');
+    status.classList.add('unread');
+    status.innerHTML = 'Unread';
+  } else {
+    status.classList.remove('unread');
+    status.classList.add('read');
+    status.innerHTML = 'Read';
+  }
+}
+
+function remove() {
+  // remove book from library array
+
+  // remove from html
 }
 
 // test books
