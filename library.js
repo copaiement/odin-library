6a8671763dcc318891a1de7a7f84c0ba222c0708
copaiement <cole.paiement@gmail.com>
2023-03-09 16:19:36 -0800

    Main structure is working. Needs some tweaks.

--- library.js
+++ library.js
@@ -1,12 +1,10 @@
 // Library JS code
 
 // event listener for add book
-const addBookBtn = document.querySelector('.add-book');
-addBookBtn.addEventListener('click', openModal);
+document.querySelector('.add-book').addEventListener('click', openModal);
 
-// event listener for submit button
-const submitBtn = document.querySelector('#submit-btn');
-submitBtn.addEventListener('click', submit);
+// event listener for submit form
+document.querySelector('#submit-btn').addEventListener('click', submit);
 
 let myLibrary = [];
 
@@ -23,9 +21,20 @@ function openModal() {
   document.querySelector('.modal').classList.remove('hidden');
 }
 
+function closeModal() {
+  document.querySelector('.add-book').classList.remove('hidden');
+  document.querySelector('.modal').classList.add('hidden');
+}
+
 function submit(event) {
-  event.preventDefault();
-  getBookInfo();
+  const form1 = document.querySelector('#title-input').reportValidity();
+  const form2 = document.querySelector('#author-input').reportValidity();
+  const form3 = document.querySelector('#pages-input').reportValidity();
+  if (form1 && form2 && form3) {
+    event.preventDefault();
+    closeModal();
+    addBook();
+  }
 }
 
 function getBookInfo() {
@@ -34,11 +43,7 @@ function getBookInfo() {
   const pages = document.querySelector('#pages-input').value;
 
   const newBook = new Book(title, author, pages);
-  myLibrary[myLibrary.length - 1] = newBook;
-
-  // hide modal and show button
-  document.querySelector('.add-book').classList.remove('hidden');
-  document.querySelector('.modal').classList.add('hidden');
+  myLibrary[myLibrary.length] = newBook;
 }
 
 function setStatus() {
@@ -52,6 +57,56 @@ function setStatus() {
     status.classList.add('read');
     status.innerHTML = 'Read';
   }
+}
+
+function addBook() {
+  // get book info
+  const titleInput = document.querySelector('#title-input').value;
+  const authorInput = document.querySelector('#author-input').value;
+  const pagesInput = document.querySelector('#pages-input').value;
+  // add book to library
+  const newBook = new Book(titleInput, authorInput, pagesInput);
+  myLibrary[myLibrary.length] = newBook;
+  // set up queries
+  const library = document.querySelector('.library-container');
+  const addBookBtn = document.querySelector('.add-book');
+  const card = document.createElement('div');
+  // insert card
+  library.insertBefore(card, addBookBtn);
+  card.classList.add('library-card');
+  // insert img child
+  const img = document.createElement('img');
+  card.appendChild(img);
+  img.classList.add('book-img');
+  img.setAttribute('src', './images/book-open-variant.svg');
+  img.setAttribute('alt', 'open book graphic');
+  // insert title child
+  const title = document.createElement('div');
+  card.appendChild(title);
+  title.classList.add('title');
+  title.textContent = `${newBook.title} by ${newBook.author}`;
+  // insert pages child
+  const pages = document.createElement('div');
+  card.appendChild(pages);
+  pages.classList.add('pages');
+  pages.textContent = `${newBook.pages} pages`;
+  // insert buttons child container
+  const btns = document.createElement('div');
+  card.appendChild(btns);
+  btns.classList.add('card-buttons');
+  // read button
+  const readBtn = document.createElement('button');
+  btns.appendChild(readBtn);
+  readBtn.classList.add('unread');
+  readBtn.setAttribute('id', 'status-btn');
+  readBtn.setAttribute('onclick', 'setStatus()');
+  readBtn.textContent = 'Unread';
+  // remove button
+  const removeBtn = document.createElement('button');
+  btns.appendChild(removeBtn);
+  removeBtn.classList.add('remove');
+  removeBtn.setAttribute('onclick', 'remove()');
+  removeBtn.textContent = 'Remove';
 }
 
 function remove() {
